292155544a7f901ad9764fc87944bb781e6efc37
EdKazik <edoskazik@gmail.com>
2023-08-14 18:06:37 +0200

    first commit task3

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,12 @@
 import './Card.css';
-import Input from '../Input/Input';
 import logo1 from '../../assets/logo1.svg';
-// import {AiOutlineEye} from 'react-icons/ai';
-import {AiOutlineEyeInvisible} from 'react-icons/ai';
+import Form from '../Form/Form';
 
 const Card = () => {
     return (
         <div className='card'>
             <div className='img'><img src={logo1} alt='logo1' /></div>
-        <div className='login__block'>
-            <Input type='text'  placeholder='   User name' className='input'/>
-            <Input type='password' placeholder='   Password' className='input' />
-            <AiOutlineEyeInvisible className='closed__eye' />
-            {/* <AiOutlineEye className='opened__eye'/> */}
-            <Input type='submit' value='Login' className='input__submit'/>
-        </div>
+            <Form/>
         </div>
     )
 };
---
+++ src/components/Products/Products.js
@@ -0,0 +1,31 @@
+import './Products.css';
+import { SlBasketLoaded } from 'react-icons/sl';
+
+
+const Products = (props) => {
+    
+    const product = props.productsList.map(elem => {
+        return(
+            <div className='product__block' key={elem.id}>
+                <div className='img'><img src={ elem.img } alt='logotype'/></div>
+                <h3>{elem.text}</h3>
+                <div className='price__quantity-block'>
+                    <div className='price'><span>{elem.price}₴</span></div>
+                    <div className='quantity'><span>Кількість: {elem.quantity}</span></div>
+                </div>
+                <div className='add__box'>
+                    <SlBasketLoaded className='basket' />
+                    <span>Готовий до відправки</span>
+                </div>
+            </div>
+    )})
+    
+
+    return (
+        <div className='products__block'>
+            {product}
+        </div>
+    )
+}
+
+export default Products;
